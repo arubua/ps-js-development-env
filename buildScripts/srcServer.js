53248d8f83ba6c2280bcf7d34969fe25b541e877
Anthony Rubua <rubua31@gmail.com>
2019-07-01 11:12:32 +0300

    error free

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', function(req,res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req,res){
+  res.json([
+    {"id":1,"firstName": "Bob", "lastName":"Smith","email":"bob@gmail.com"},
+    {"id":2,"firstName": "Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
+    {"id":3,"firstName": "Ian", "lastName": "Kamau", "email": "mauian@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -8,15 +8,15 @@ describe('Our first test',() => {
   });
 });
 
-describe('index.html', () => {
-  it ('should say hello', (done) => {
-    const index = fs.readFileSync ('./src/index.html', 'utf-8');
-    jsdom.env(index, function (err, window){
-      const h1 = window.getElementByTagName('h1') [0];
-      expect (h1.innerHTML).to.equal("Hello World!");
-      done ();
+ describe('index.html', () => {
+  it ('should have h1 that says Users', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName('h1') [0];
+      expect(h1.innerHTML).to.equal("Users");
+      done();
       window.close();
      });
 
   });
-})
+});
