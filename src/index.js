441b5c7afb4567b9991630c90d12dc629622971d
Anthony Rubua <rubua31@gmail.com>
2019-07-03 10:24:10 +0300

    Bundled with vendoe and main

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getUsers } from './api/userApi';
+import { getUsers,deleteUser } from './api/userApi';
 
 //Populate table of users via API call
 getUsers().then(result => {
@@ -14,5 +14,20 @@ getUsers().then(result => {
     </tr> `
   });
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //Must use array.from to create a real array from a DOM collection
+  //getElementsByClassName only returns an 'array like' object
+
+  Array.from(deleteLinks,link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
 
---
+++ webpack.config.prod.js
@@ -0,0 +1,53 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor:path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  plugins: [
+    /*Use CommonsChunkPlugin to create separate bundle
+    of vendor libraries so that they are cached separately */
+    new webpack.optimize.CommonsChunkPlugin({
+      name:"vendor"
+    }),
+    //Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin ({
+      template:'src/index.html',
+      minify:{
+        removeComments:true,
+        collapseWhitespace: true,
+        removeRedundantAttributes : true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject:true
+    }),
+  //Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    //minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
