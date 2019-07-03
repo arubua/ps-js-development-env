cd1de4e1ac2f2d93b42de0c53c97c6d9f108b88a
Anthony Rubua <rubua31@gmail.com>
2019-07-03 13:53:08 +0300

    Bundled and split....deployment ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import { getUsers,deleteUser } from './api/userApi';
 
 //Populate table of users via API call
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,36 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
   entry: {
-    vendor:path.resolve(__dirname, 'src/vendor'),
+    vendor: path.resolve(__dirname, 'src/vendor'),
     main: path.resolve(__dirname, 'src/index')
   },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //Generate an external css file with a hash in the filename.
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    //Hash the files using MD5 so that their names change when the content changes
+    new WebpackMd5Hash(),
+
     /*Use CommonsChunkPlugin to create separate bundle
     of vendor libraries so that they are cached separately */
     new webpack.optimize.CommonsChunkPlugin({
-      name:"vendor"
+      name: 'vendor'
     }),
+
     //Create HTML file that includes reference to bundled JS
     new HtmlWebpackPlugin ({
       template:'src/index.html',
@@ -37,7 +46,11 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-      inject:true
+      inject:true,
+      //Properties defined here are available in index.html
+      //using htmlWebpackPlugin.options.varName
+      trackJSToken: '2736df422ee94f15a9fc2edafdd58bd4'
+
     }),
   //Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
@@ -47,7 +60,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
     ]
   }
-}
+};
